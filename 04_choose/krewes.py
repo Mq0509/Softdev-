0f0cd2a0ad74e218e51d735c8bf6d9fb102d86dc
May Qiu <90876910+Mq0509@users.noreply.github.com>
2022-09-28 08:18:41 -0400

    Update krewes.py

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -5,12 +5,28 @@ K04 -- krewes/Making a random generator to return the name of a student from any
 2022-09-23
 .3 hrs
 
-OP Summary
-We decided to divide the random portion into two parts.
-We first wanted to randomly choose a key and then after we did so, we choose a devo from that key.
-So, we created a list of those keys by using the .keys method and by turning it into a list.
-We then used random.randint in order to choose a random index in that list.
-After choosing a random key, we redid our steps to find a random devo within that period.
+OP Summary for randomdevo()
+1) Create list of the keys in krewes
+2) Choose a random key / period in the list
+3) Create list of the devos in the selected key / period
+4) Choose a random devo in the period
+5) Print the selected devo's name and period
+
+OP Summary for morethanone()
+1) Create variable numberofdevos to take user input
+2) Check if numberofdevos is within range (1 < numberofdevos < 106)
+   If so, proceed.
+   Else, user selects new numbers until there is one in range.
+3) Create for loop that continues until the required number of devos are printed.
+   Inside the for loop: ( steps a-e are the same as randomdevo() )
+   a) Create list of the keys in krewes
+   b) Choose a random key / period in the list
+   c) Create list of the devos in the selected key / period
+   d) Choose a random devo in the period
+   e) Print the selected devo's name and period
+   f) Check if the list of devos is of length 1
+      If so, remove key / period from krewes
+      Else, continue in the loop
 
 Q/C/C
 What other things can dictionaries do?
@@ -22,7 +38,7 @@ We needed to cast the .keys into a list otherwise we got the Type Error: "dict_k
 '''
 import random
 krewes = {
-           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"], 
+           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"],
            7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
            8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
          }
