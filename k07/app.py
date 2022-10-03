d907946c544d21fadb32feb49f4966c9d7ae4e97
May Qiu <90876910+Mq0509@users.noreply.github.com>
2022-10-03 13:19:27 -0400

    Update app.py

--- k07/app.py
+++ k07/app.py
@@ -3,8 +3,8 @@
 from flask import Flask
 
 app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
-#
-@app.route("/") # Q1: What points of reference do you have for meaning of '/'?
+
+@app.route("/") # Q1: What points of reference do you have for meaning of '/'?    
 def hello_world():
     print(__name__) # Q2: Where will this print to? Q3: What will it print?
     return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?
@@ -16,11 +16,11 @@ app.run()  # Q5: Where have you seen similar constructs in other languages?
 DISCO:
 
 QCC:
-0. 
-1. 
+0. Creating an object in Java
+1. math dividing -Bri
 2. 
 3. 
-4. 
+4. In Spain -Gitae
 5. 
 ...
 
